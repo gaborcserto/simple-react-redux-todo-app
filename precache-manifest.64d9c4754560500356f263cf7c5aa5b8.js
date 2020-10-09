15bc7bf3e1298a71abf1cc3e60cdc73aaafaf393
Gabor Cserto <csertog@gmail.com>
2020-10-09 14:46:14 +0200

    Updates

---
+++ precache-manifest.64d9c4754560500356f263cf7c5aa5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cf853308ed6580ff5d13a8ecef86cf9",
+    "url": "/simple-react-redux-todo-app/index.html"
+  },
+  {
+    "revision": "bbb42624d9804eb1a1fd",
+    "url": "/simple-react-redux-todo-app/static/css/main.3bea8f39.chunk.css"
+  },
+  {
+    "revision": "1a444227f7b635d3d442",
+    "url": "/simple-react-redux-todo-app/static/js/2.d40a051a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simple-react-redux-todo-app/static/js/2.d40a051a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb42624d9804eb1a1fd",
+    "url": "/simple-react-redux-todo-app/static/js/main.a41e4433.chunk.js"
+  },
+  {
+    "revision": "91b00f9902a74365f06e",
+    "url": "/simple-react-redux-todo-app/static/js/runtime-main.061171e5.js"
+  }
+]);
